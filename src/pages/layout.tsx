4a28b0303f6ac8e78901047b91e34015881855d9
CGH <tt878429@gmail.com>
2023-06-11 22:30:55 +0900

    공통 레이아웃 구현 및 페이지 라우팅

---
+++ src/pages/layout.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { breakpoints } from "styles/breakPoints";
+
+import { Outlet } from "react-router-dom";
+import Header from "components/Header/Header";
+
+const Main = styled.main`
+  padding-bottom: 200px;
+
+  @media ${breakpoints.md} {
+    padding-top: 100px;
+  }
+`;
+
+function RootLayout() {
+  return (
+    <>
+      <Header />
+      <Main>
+        <Outlet />
+      </Main>
+    </>
+  );
+}
+
+export default RootLayout;
