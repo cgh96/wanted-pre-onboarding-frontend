d1773fc15463e04f0adb9617160a05a919590b09
CGH <tt878429@gmail.com>
2023-06-13 16:10:25 +0900

    Fix: email 입력창 type을 email에서 text로 변경

--- src/components/Auth/AuthForm.tsx
+++ src/components/Auth/AuthForm.tsx
@@ -60,7 +60,7 @@ function AuthForm() {
           testId="email-input"
           handleValue={handleEmail}
           value={email}
-          type="email"
+          type="text"
           title="이메일"
           validation={emailValidation}
         />
